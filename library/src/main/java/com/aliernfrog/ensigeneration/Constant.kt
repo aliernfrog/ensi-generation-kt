8458216d8242c03cefa96504574ad2e2dd845242
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-11-05 09:11:27 +0300

    feat: `EnsiConfigDefaults`

--- library/src/main/java/com/aliernfrog/ensigeneration/Constant.kt
+++ library/src/main/java/com/aliernfrog/ensigeneration/Constant.kt
@@ -19,3 +19,22 @@ object EnsiGenerationType {
      */
     const val ALLCAPS = "ALLCAPS"
 }
+
+/**
+ * Default values for [EnsiConfig]
+ */
+object EnsiConfigDefaults {
+    val words = setOf("me","you","we","they","alierns","indinibee","bees","momes","frogs","mouse","chicken","furries","frog","Exi's basement","free candies","ensi","van","laptop","marchmilos","mouse")
+    val verbs = setOf("sobbed","adsed","feed","featured","faced","undefined","petted","mousing")
+    val times = setOf("one day","weeks ago","yesterday","years ago","tomorrow")
+    val chars = setOf("frog","mouse","Ensi","Aliern","Exi","Infini","marchmilo","cat","memer","karen","manager","bro","man","you","he","she","it","they","that")
+    val places = setOf("hospital","basement","hideout","hotel","parking","shop","chinatina")
+    val concs = setOf("and","but","when","even though","before","after","while","when")
+    val emotions = setOf("what the fuck","fuck","shit","woow","wow","oh god","omg","omfg")
+    val others = setOf("unfortunately","fortunately","luckily","finally","thankfully","at least","weirdly","in fact","actually","fr","for real")
+    val positions = setOf("in %","at %","on %","behind %","under %","between % and %")
+    // TYPES
+    val normalTypes = setOf("%WORD_VERB%","%WORD_COUNTED%","%CHARS% was %WORD_VERB%","%CHARS% %EDVERB%","%VERB% %CHARS%")
+    val questionTypes = setOf("was %CHARS% %VERB%?","was %CHARS% %INGVERB% %CHARS%?","was %CHARS% %INGVERB% %WORD_COUNTED%?","was %CHARS% %WORD_COUNTED%?")
+    val startingTypes = setOf("%TIME%, ","%TIME% %PLACE%, ","%PLACE%, ")
+}
--- library/src/main/java/com/aliernfrog/ensigeneration/EnsiConfig.kt
+++ library/src/main/java/com/aliernfrog/ensigeneration/EnsiConfig.kt
@@ -1,19 +1,19 @@
 package com.aliernfrog.ensigeneration
 
 data class EnsiConfig(
-    var words: Set<String> = setOf("me","you","we","they","alierns","indinibee","bees","momes","frogs","mouse","chicken","furries","frog","Exi's basement","free candies","ensi","van","laptop","marchmilos","mouse"),
-    var verbs: Set<String> = setOf("sobbed","adsed","feed","featured","faced","undefined","petted","mousing"),
+    var words: Set<String> = EnsiConfigDefaults.words,
+    var verbs: Set<String> = EnsiConfigDefaults.verbs,
     var edVerbs: Set<String> = verbs.filter { it.lowercase().endsWith("ed") }.toSet(),
     var ingVerbs: Set<String> = verbs.filter { it.lowercase().endsWith("ing") }.toSet(),
-    val times: Set<String> = setOf("one day","weeks ago","yesterday","years ago","tomorrow"),
-    val chars: Set<String> = setOf("frog","mouse","Ensi","Aliern","Exi","Infini","marchmilo","cat","memer","karen","manager","bro","man","you","he","she","it","they","that"),
-    val places: Set<String> = setOf("hospital","basement","hideout","hotel","parking","shop","chinatina"),
-    val concs: Set<String> = setOf("and","but","when","even though","before","after","while","when"),
-    val emotions: Set<String> = setOf("what the fuck","fuck","shit","woow","wow","oh god","omg","omfg"),
-    val others: Set<String> = setOf("unfortunately","fortunately","luckily","finally","thankfully","at least","weirdly","in fact","actually","fr","for real"),
-    val positions: Set<String> = setOf("in %","at %","on %","behind %","under %","between % and %"),
+    val times: Set<String> = EnsiConfigDefaults.times,
+    val chars: Set<String> = EnsiConfigDefaults.chars,
+    val places: Set<String> = EnsiConfigDefaults.places,
+    val concs: Set<String> = EnsiConfigDefaults.concs,
+    val emotions: Set<String> = EnsiConfigDefaults.emotions,
+    val others: Set<String> = EnsiConfigDefaults.others,
+    val positions: Set<String> = EnsiConfigDefaults.positions,
     // TYPES
-    val normalTypes: Set<String> = setOf("%WORD_VERB%","%WORD_COUNTED%","%CHARS% was %WORD_VERB%","%CHARS% %EDVERB%","%VERB% %CHARS%"),
-    val questionTypes: Set<String> = setOf("was %CHARS% %VERB%?","was %CHARS% %INGVERB% %CHARS%?","was %CHARS% %INGVERB% %WORD_COUNTED%?","was %CHARS% %WORD_COUNTED%?"),
-    val startingTypes: Set<String> = setOf("%TIME%, ","%TIME% %PLACE%, ","%PLACE%, ")
+    val normalTypes: Set<String> = EnsiConfigDefaults.normalTypes,
+    val questionTypes: Set<String> = EnsiConfigDefaults.questionTypes,
+    val startingTypes: Set<String> = EnsiConfigDefaults.startingTypes
 )
