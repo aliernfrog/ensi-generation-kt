03051f516f72fb8d2096ccfc5418fc6b241de996
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-11-05 09:32:23 +0300

    fix: fallback to verbs if empty

--- library/src/main/java/com/aliernfrog/ensigeneration/EnsiConfig.kt
+++ library/src/main/java/com/aliernfrog/ensigeneration/EnsiConfig.kt
@@ -3,8 +3,8 @@ package com.aliernfrog.ensigeneration
 data class EnsiConfig(
     var words: Set<String> = EnsiConfigDefaults.words,
     var verbs: Set<String> = EnsiConfigDefaults.verbs,
-    var edVerbs: Set<String> = verbs.filter { it.lowercase().endsWith("ed") }.toSet(),
-    var ingVerbs: Set<String> = verbs.filter { it.lowercase().endsWith("ing") }.toSet(),
+    var edVerbs: Set<String> = verbs.filter { it.lowercase().endsWith("ed") }.toSet().ifEmpty { verbs },
+    var ingVerbs: Set<String> = verbs.filter { it.lowercase().endsWith("ing") }.toSet().ifEmpty { verbs },
     val times: Set<String> = EnsiConfigDefaults.times,
     val chars: Set<String> = EnsiConfigDefaults.chars,
     val places: Set<String> = EnsiConfigDefaults.places,
